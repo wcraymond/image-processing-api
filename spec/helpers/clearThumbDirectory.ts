0cb225e2f8eef70bd5cc0d5af4bc8b42c878c745
William Raymond <william.c.raymond@protonmail.com>
2022-06-24 13:33:23 -0400

    chore: Add code attribution

--- spec/helpers/clearThumbDirectory.ts
+++ spec/helpers/clearThumbDirectory.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 
+/* Inspired by the following StackOverflow post:
+ * https://stackoverflow.com/questions/27072866/how-to-remove-all-files-from-directory-without-removing-directory-in-node-js */
 const clearThumbDirectory = (): void => {
   const thumbdir = path.join(__dirname, '../../../assets/thumb');
   fs.readdir(thumbdir, (err, files) => {
